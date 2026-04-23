9bd16de24646e3eb7dacf5c37563c153cd45f06b
Júnior Messias <jrmessias@gmail.com>
2026-04-22 23:48:14 -0300

    ✨ chore: remove unused Section2 import from About.js
    Clean up: drop unused Section2 import on About page to prevent unused dependency and potential lint issues.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Layout from "../../components/Layouts/Layout";
 import "../../styles/HomeStyle.css";
-import Section2 from "../Home/Section2";
 import StorySection from "./StorySection";
 
 function About() {
